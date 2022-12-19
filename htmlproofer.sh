e88f44072c573248b9c950399f6525cde8282cb0
Greg Walker <greg.walker@gsa.gov>
2022-12-19 17:49:47 -0600

    mebbe annotations

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-htmlproofer $1 --disable-external --allow-missing-href
+htmlproofer $1 --disable-external --allow-missing-href 2>raw-errors.txt > /dev/null
+CODE=$?
+
+if [ "$CODE" -eq "0" ]; then
+  exit 0
+fi
+
+# Strip the parts of html-proofer output we don't care about
+head -n -2 raw-errors.txt | tail +2 > errors.txt
+
+while read LINE; do
+  if [[ "$LINE" =~ ^\*[[:space:]]At[[:space:]]([^:]*) ]]; then
+    PAGE="${BASH_REMATCH[1]}"
+  elif [[ ! "$LINE" =~ ^$ ]]; then
+    echo "::error title=$LINE::in $PAGE"
+  fi
+done < errors.txt
+
+exit 1
