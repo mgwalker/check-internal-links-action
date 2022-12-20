ea27cf8fff2fcfbf330019d1ecfe7c705ae377fd
Greg Walker <greg.walker@gsa.gov>
2022-12-19 18:31:38 -0600

    mebbe annotations

--- htmlproofer.sh
+++ htmlproofer.sh
@@ -9,13 +9,18 @@ if [ "$CODE" -eq "0" ]; then
   exit 0
 fi
 
-# Strip the parts of html-proofer output we don't care about
-head -n -2 raw-errors.txt | tail +2 > errors.txt
+# html-proofer output begins with a type-of-test header and ends with
+head -n -2 raw-errors.txt > errors.txt
 
 while read LINE; do
   if [[ "$LINE" =~ ^\*[[:space:]]At[[:space:]]([^:]*) ]]; then
+    # This line indicates a file with an error in it, so save off the page name
     PAGE="${BASH_REMATCH[1]}"
+  elif [[ "$LINE" =~ ^For[[:space:]]the ]]; then
+    # This line indicates the start of a new type-of-test. We don't care.
+    :
   elif [[ ! "$LINE" =~ ^$ ]]; then
+    # This line is not empty, so it must be error text. Now we can output it!
     echo "::error title=${LINE//,}::in ${PAGE//,}"
   fi
 done < errors.txt
